76af9a8d9d2e04b2cfba7f70741be85ad0d990ec
Haaris Mian <mail@haarismian.com>
2021-01-07 03:36:12 -0500

    server done api works

--- server/server.js
+++ server/server.js
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to node application from haaris.' });
 });
 
+require('./routes/tutorial.routes')(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
